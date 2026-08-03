e09484962cc69262490e0edb18803ffbeececad3
bookieandco <morrisdorian84@gmail.com>
2026-08-03 17:07:09 -0500

    chore: scaffold jhadina-web home page

---
+++ apps/jhadina-web/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Jhadina - Personal AI Operating System</h1>
+      <p>Loading...</p>
+    </main>
+  );
+}
