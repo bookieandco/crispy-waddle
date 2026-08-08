118b8f5a2cfa4604f7a3139569ef366fe6e4e5db
bookieandco <morrisdorian84@gmail.com>
2026-08-08 15:43:53 -0500

    feat(music): export search and taste APIs

--- packages/music-core/src/index.ts
+++ packages/music-core/src/index.ts
@@ -1,3 +1,6 @@
 export * from "./types.js";
 export * from "./repository.js";
 export * from "./matching.js";
+export * from "./taste.js";
+export * from "./youtube-music.js";
+export * from "./search.js";
