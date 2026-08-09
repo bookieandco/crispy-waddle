9be4d62291bf32864d4ccbcf21ebdad5996263d7
bookieandco <morrisdorian84@gmail.com>
2026-08-09 13:06:08 -0500

    feat(shotlist): export production and recipe contracts

--- packages/shotlist-core/src/index.ts
+++ packages/shotlist-core/src/index.ts
@@ -3,3 +3,5 @@ export * from "./emit.js";
 export * from "./polish.js";
 export * from "./assembly.js";
 export * from "./external-adapters.js";
+export * from "./production.js";
+export * from "./recipes.js";
