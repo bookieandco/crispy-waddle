1d4a7b962cc3b80c77e6451fe006bf308594a65f
bookieandco <morrisdorian84@gmail.com>
2026-08-09 18:24:10 -0500

    feat(jhadina): export core memory and audit bridges

--- packages/jhadina-integration/src/index.ts
+++ packages/jhadina-integration/src/index.ts
@@ -3,3 +3,5 @@ export * from './event-bus';
 export * from './orchestrator';
 export * from './system-health';
 export * from './core-adapters';
+export * from './memory-adapter';
+export * from './audit-adapter';
