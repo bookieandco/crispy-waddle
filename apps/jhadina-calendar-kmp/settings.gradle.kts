36900d8743c01e00a22e82ecc68f786972f4a3fc
bookieandco <morrisdorian84@gmail.com>
2026-08-10 12:21:00 -0500

    build(jhadina-calendar): add standalone KMP plugin management

---
+++ apps/jhadina-calendar-kmp/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    plugins {
+        kotlin("multiplatform") version "2.3.0"
+        id("com.android.library") version "8.13.2"
+        id("org.jetbrains.compose") version "1.10.0"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "jhadina-calendar-kmp"
+include(":calendar")
