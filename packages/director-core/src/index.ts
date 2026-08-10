a2ddae44d13e3729fa76edda03c1d13d9b12b970
bookieandco <morrisdorian84@gmail.com>
2026-08-10 14:16:51 -0500

    fix(director-core): restore package root exports for Vercel

---
+++ packages/director-core/src/index.ts
@@ -0,0 +1,6 @@
+export * from './candidate-ranking.js';
+export * from './continuity-manifest.js';
+export * from './continuity-qc.js';
+export * from './timeline-command.js';
+export * from './timeline-editing.js';
+export * from './timeline-model.js';
