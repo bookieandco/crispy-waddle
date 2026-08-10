0410e9fc4d65af126724aabc46c37662e7e4845d
bookieandco <morrisdorian84@gmail.com>
2026-08-09 20:39:31 -0500

    feat(studio): register behavior runtime handler

--- apps/jhadina-web/src/lib/studio/register-handlers.ts
+++ apps/jhadina-web/src/lib/studio/register-handlers.ts
@@ -1,6 +1,7 @@
 import { StudioHandlerRegistry } from "./action-handlers";
 import { CharacterInteractionHandler } from "./handlers/character-interaction-handler";
 import { InteractionAnimationHandler } from "./handlers/animation-handler";
+import { BehaviorRuntimeHandler } from "./handlers/behavior-runtime-handler";
 import { QCHandler } from "./handlers/qc-handler";
 import { VoiceSyncHandler } from "./handlers/voice-sync-handler";
 import { AnimationHandler, CharacterReplacementHandler, RenderHandler, RigHandler } from "./handlers/visual-handlers";
@@ -11,6 +12,7 @@ export function createStudioHandlerRegistry(): StudioHandlerRegistry {
   registry.register(new QCHandler());
   registry.register(new CharacterInteractionHandler());
   registry.register(new InteractionAnimationHandler());
+  registry.register(new BehaviorRuntimeHandler());
   registry.register(new CharacterReplacementHandler());
   registry.register(new RigHandler());
   registry.register(new AnimationHandler());
