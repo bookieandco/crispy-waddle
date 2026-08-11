0e7c233cf28f526c81886196a583a5b47badf14d
bookieandco <morrisdorian84@gmail.com>
2026-08-10 19:20:14 -0500

    fix(director): export sfx provider contract

--- packages/director-core/src/index.ts
+++ packages/director-core/src/index.ts
@@ -1,6 +1,7 @@
 export * from './candidate-ranking.js';
 export * from './continuity-manifest.js';
 export * from './continuity-qc.js';
+export * from './sfx-provider.js';
 export * from './timeline-command.js';
 export * from './timeline-editing.js';
 export * from './timeline-model.js';
