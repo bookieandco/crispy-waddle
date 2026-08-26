a7586bd03bcac0bc10f791c95ef818751399b9ee
bookieandco <morrisdorian84@gmail.com>
2026-08-25 21:17:27 -0500

    feat(jhadinatv): export channel programming engine

---
+++ packages/jhadina-tv-programming/src/index.ts
@@ -0,0 +1,8 @@
+export type {
+  JhadinaChannelDefinition,
+  JhadinaChannelEngine,
+  JhadinaChannelSchedule,
+  JhadinaScheduledItem,
+} from '@jhadina/tv-core';
+
+export { createJhadinaChannelEngine } from '@jhadina/tv-core';
