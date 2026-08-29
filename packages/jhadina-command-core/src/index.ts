20d3f7a62e63391889c35fd438631e722f4203b0
bookieandco <morrisdorian84@gmail.com>
2026-08-29 13:36:05 -0500

    feat(command): export registry command gateway

--- packages/jhadina-command-core/src/index.ts
+++ packages/jhadina-command-core/src/index.ts
@@ -3,3 +3,4 @@ export * from "./capability-planner";
 export * from "./capability-adapter";
 export * from "./command-entry";
 export * from "./look-command-planner";
+export * from "./registry-command-gateway";
