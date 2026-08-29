4d145176e9b28c4e6547da44b6a4baadcb2d19fb
bookieandco <morrisdorian84@gmail.com>
2026-08-29 11:19:05 -0500

    feat(jhadina-learn): export experience reflection layer

--- packages/jhadina-learn-core/src/index.ts
+++ packages/jhadina-learn-core/src/index.ts
@@ -2,3 +2,5 @@ export * from "./learning-contract";
 export * from "./learning-boundary";
 export * from "./source-normalizer";
 export * from "./in-memory-learning-repository";
+export * from "./experience-contract";
+export * from "./experience-boundary";
