697fc218ffc4b6cdcca554a7a5e054880bd796a2
bookieandco <morrisdorian84@gmail.com>
2026-08-29 10:20:17 -0500

    feat(shark): export shark 0 foundation contracts

---
+++ packages/shark-core/src/index.ts
@@ -0,0 +1,5 @@
+export * from './wallets/MultiChainWallet';
+export * from './observations/SharkObservation';
+export * from './decisions/SharkDecision';
+export * from './outcomes/SharkOutcome';
+export * from './learning/SharkLearning';
