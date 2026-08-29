4afd05263f8c7fa83813ac64ec660cef499b4b6d
bookieandco <morrisdorian84@gmail.com>
2026-08-29 10:22:31 -0500

    feat(shark): export entity graph foundation

--- packages/shark-core/src/index.ts
+++ packages/shark-core/src/index.ts
@@ -3,3 +3,5 @@ export * from './observations/SharkObservation';
 export * from './decisions/SharkDecision';
 export * from './outcomes/SharkOutcome';
 export * from './learning/SharkLearning';
+export * from './graph/SharkEntityGraph';
+export * from './graph/SharkGraphBuilder';
