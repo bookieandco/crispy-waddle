0dca432b45e0957bae5d2b73e10c813cd1008d8e
bookieandco <morrisdorian84@gmail.com>
2026-08-30 16:46:55 -0500

    feat(real-core): expose humor core

--- packages/jhadina-entertainment-core/src/index.ts
+++ packages/jhadina-entertainment-core/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./domain";
 export * from "./engine";
+export * from "./humor";
 export * from "./graph/types.js";
 export * from "./graph/creative-knowledge-graph.js";
 export * from "./reference/index.js";
