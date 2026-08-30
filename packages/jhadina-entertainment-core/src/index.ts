6e260fd6df2280b25f0a5739b2732796446e7a32
bookieandco <morrisdorian84@gmail.com>
2026-08-30 18:23:05 -0500

    feat(research): export web scout pipeline

--- packages/jhadina-entertainment-core/src/index.ts
+++ packages/jhadina-entertainment-core/src/index.ts
@@ -15,6 +15,7 @@ export * from "./culture-scheduler";
 export * from "./voice-core";
 export * from "./voice-engine";
 export * from "./web-scout";
+export * from "./web-scout-pipeline";
 export * from "./oasis-social-reference";
 export * from "./graph/types.js";
 export * from "./graph/creative-knowledge-graph.js";
