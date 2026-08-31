5995326766381a8728f62b2a48d9b6fae0055463
bookieandco <morrisdorian84@gmail.com>
2026-08-31 10:14:31 -0500

    feat(core-spine): export governed action gateway

--- packages/jhadina-core-spine/src/index.ts
+++ packages/jhadina-core-spine/src/index.ts
@@ -1,5 +1,6 @@
 export * from './spine.js';
 export * from './types.js';
+export * from './action-gateway.js';
 export * from './evolution.js';
 export * from './emergency-domain.js';
 export * from './emergency-evidence.js';
