4fe981a7f88a60fdbe184a6313cfc306d1598eea
bookieandco <morrisdorian84@gmail.com>
2026-08-31 14:13:17 -0500

    feat(media): export browser playback boundary

--- packages/jhadina-tv-core/src/index.ts
+++ packages/jhadina-tv-core/src/index.ts
@@ -21,6 +21,7 @@ export type { JhadinaTVReceiverTransport } from './cast/jhadina-tv-receiver'; ex
 export type { AirPlayVideo } from './cast/browser-airplay'; export { createBrowserAirPlayController } from './cast/browser-airplay';
 export type { PictureInPictureController, PictureInPictureDocument, PictureInPictureVideo } from './picture-in-picture'; export { createPictureInPictureController } from './picture-in-picture';
 export type { LocalPlaybackAdapter, UnifiedMediaSession, UnifiedMediaSessionConfig } from './media-session'; export { createUnifiedMediaSession } from './media-session';
+export type { BrowserMediaElement, BrowserPlaybackHost } from './browser-playback'; export { createBrowserPlaybackHost } from './browser-playback';
 export type { PlaybackAdapterKind, PlaybackAdapterContext, PlaybackAdapter, DirectSourcePlaybackAdapter, YouTubePlaybackAdapter, RemotePlaybackAdapter, PlaybackResolver, PlaybackSessionFactory, ResolvedPlaybackSession, PlaybackSnapshot } from './playback'; export { createPlaybackResolver } from './playback';
 export { resolvePlaybackAdapter, createDirectSourceAdapter, createYouTubePlaybackAdapter } from './playback-adapters';
 export type { MediaPlaybackProgress, MediaPlaybackProgressRepository } from './media-playback-persistence'; export { InMemoryMediaPlaybackProgressRepository, isMeaningfulResume } from './media-playback-persistence';
