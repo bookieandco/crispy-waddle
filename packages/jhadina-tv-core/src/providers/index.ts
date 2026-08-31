90d6f4e79597e4e431bffdda54a347fda82f2fea
bookieandco <morrisdorian84@gmail.com>
2026-08-31 09:52:00 -0500

    feat(media): expose provider runtime

---
+++ packages/jhadina-tv-core/src/providers/index.ts
@@ -0,0 +1,4 @@
+export { createYouTubeProvider } from './youtube';
+export type { YouTubeProviderConfig, YouTubeSearchResponse } from './youtube';
+export { createMediaProviderRegistry } from './runtime';
+export type { MediaProviderRuntimeConfig } from './runtime';
