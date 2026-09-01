3524f0803004034593a41f6b34f81ca9bca79e66
bookieandco <morrisdorian84@gmail.com>
2026-08-31 21:48:40 -0500

    feat(gaming-core): export gameboy runtime host

--- packages/gaming-core/src/index.ts
+++ packages/gaming-core/src/index.ts
@@ -3,6 +3,7 @@ export * from './hid-adapter.js';
 export * from './runtime.js';
 export * from './runtime-registry.js';
 export * from './gameboy-runtime.js';
+export * from './gameboy-runtime-host.js';
 export * from './game-library.js';
 export * from './save.js';
 export * from './jhadina-gaming.js';
