950406d3a52108f641667e65287a6a1769792dc4
bookieandco <morrisdorian84@gmail.com>
2026-08-31 21:46:41 -0500

    feat(gaming-core): export game library

--- packages/gaming-core/src/index.ts
+++ packages/gaming-core/src/index.ts
@@ -3,4 +3,5 @@ export * from './hid-adapter.js';
 export * from './runtime.js';
 export * from './runtime-registry.js';
 export * from './gameboy-runtime.js';
+export * from './game-library.js';
 export * from './jhadina-gaming.js';
