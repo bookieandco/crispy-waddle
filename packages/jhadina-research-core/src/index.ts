5a2ab262a66430cc86bbbfa3854ec91c4d3b90ce
bookieandco <morrisdorian84@gmail.com>
2026-09-20 09:49:37 -0700

    feat(research): export reconciled research runtime

--- packages/jhadina-research-core/src/index.ts
+++ packages/jhadina-research-core/src/index.ts
@@ -1,2 +1,6 @@
 export * from "./research-queue.js";
 export * from "./runtime-reconciliation.js";
+export * from "./runtime-repository.js";
+export * from "./research-executor.js";
+export * from "./knowledge-candidate.js";
+export * from "./policy-normalization.js";
