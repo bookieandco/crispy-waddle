26f70e2b3d17cdae29d3386f054d5b623797f89c
bookieandco <morrisdorian84@gmail.com>
2026-09-19 17:45:29 -0700

    JLLM-05: export provider adapter layer

--- packages/jhadina-intelligence-core/src/index.ts
+++ packages/jhadina-intelligence-core/src/index.ts
@@ -5,3 +5,4 @@ export * from './intelligence-fabric.js';
 export * from './model-registry.js';
 export * from './task-classifier.js';
 export * from './capability-aware-model-router.js';
+export * from './provider-adapter.js';
