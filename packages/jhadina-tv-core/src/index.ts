e4c3451aca6622e3a95b49caba58ac289ed6b354
bookieandco <morrisdorian84@gmail.com>
2026-09-19 17:35:36 -0700

    feat(tv): export media source request

--- packages/jhadina-tv-core/src/index.ts
+++ packages/jhadina-tv-core/src/index.ts
@@ -96,7 +96,7 @@ export const JHADINA_TV_ROUTES = {
   watch: (kind: MediaKind, id: string) => `/jhadinatv/watch/${kind}/${id}`,
 };
 
-export type { MediaRight, MediaSource, MediaSourceAdapter, MediaSourceAuthorization } from './source-adapter';
+export type { MediaRight, MediaSource, MediaSourceAdapter, MediaSourceAuthorization, MediaSourceRequest } from './source-adapter';
 export { assertAuthorizedSource, assertMediaRight, assertPlayableSource } from './source-adapter';
 export type { CastingManager, MediaSessionCommand, MediaSessionController, MediaSessionState, PlaybackTarget, PlaybackTransport } from './casting';
 export { assertCastableSource, buildTransferCommand, createCastingManager } from './casting';
