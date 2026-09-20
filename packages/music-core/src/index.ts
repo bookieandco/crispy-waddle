e8bacd9b1273a24e83ee4ddf499cb94ea5f90fa0
bookieandco <morrisdorian84@gmail.com>
2026-09-19 20:44:44 -0700

    JLLM-18J: export Jhadina media intelligence ingress

--- packages/music-core/src/index.ts
+++ packages/music-core/src/index.ts
@@ -19,3 +19,4 @@ export * from "./realtime-audio-engine.js";
 export * from "./studio-adapter.js";
 export * from "./mobile-adapter.js";
 export * from "./restoration-engine/index.js";
+export * from "./jhadina-media-intelligence-ingress.js";
