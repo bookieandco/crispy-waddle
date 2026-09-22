b4304115625d8864e6d24038de0c0756b10d4e18
bookieandco <morrisdorian84@gmail.com>
2026-09-22 12:46:45 -0700

    feat(director): export commercial creative and product bootstrap contracts

--- packages/director-core/src/index.ts
+++ packages/director-core/src/index.ts
@@ -104,3 +104,5 @@ export * from './character-reference-bootstrap';
 export * from './generative-extend';
 export * from './reference-character-video';
 export * from './character-identity-qc';
+export * from './commercial-creative-lab';
+export * from './product-reference-bootstrap';
