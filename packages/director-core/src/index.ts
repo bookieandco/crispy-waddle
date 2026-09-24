59ea8cefe2e749e81ae59b4ec79f4a5c9cc783a1
bookieandco <morrisdorian84@gmail.com>
2026-09-23 22:32:26 -0700

    feat(director): carry index.ts character training support

--- packages/director-core/src/index.ts
+++ packages/director-core/src/index.ts
@@ -108,6 +108,7 @@ export * from './cast-bible';
 export * from './voice-identity';
 export * from './movie-audio-bible';
 export * from './character-reference-bootstrap';
+export * from './character-training-pipeline';
 export * from './generative-extend';
 export * from './reference-character-video';
 export * from './character-identity-qc';
